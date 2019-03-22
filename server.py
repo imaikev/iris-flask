8d1aa25474afc4998f4376bdc1581be3442b6917
imaikev <47906773+imaikev@users.noreply.github.com>
2019-03-22 10:45:58 -0300

    Create server.py

---
+++ server.py
@@ -0,0 +1,21 @@
+import pickle
+import numpy as np
+import pandas as pd
+from flask import Flask, jsonify, request
+
+
+rfc = pickle.load(open('iris_rfc.pkl', 'rb'))
+app = Flask(__name__)
+
+@app.route('/api', methods=['POST'])
+def make_predict():
+    data = request.get_json(force=True)
+    predict_request = [data['sl'], data['sw'], data['pl'], data['pw']]
+    predict_request = np.array(predict_request)
+    predict_request = predict_request.reshape(1, -1)
+    predictions = rfc.predict(predict_request)
+
+    return jsonify(result=predictions.tolist())
+
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
